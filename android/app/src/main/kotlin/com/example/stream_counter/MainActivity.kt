4dd935e31e7acc50f9e8f650af29e86c2155b335
U190709050 <elf.prlk64@gmail.com>
2021-09-12 20:46:46 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/stream_counter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stream_counter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Stream Ile Counter',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: CounterPage(),
+    );
+  }
+}
+
+class CounterPage extends StatefulWidget {
+  @override
+  _CounterPageState createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int _counter = 0;
+  final StreamController<int> _streamController = StreamController<int>();
+
+  @override
+  void dispose() {
+    _streamController.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Stream Ile Counter')),
+      body: Center(
+        child: StreamBuilder<int>(
+            stream: _streamController.stream,
+            initialData: _counter,
+            builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+              return Text('Sayaç: ${snapshot.data}');
+            }),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          _streamController.sink.add(++_counter);
+        },
+      ),
+    );
+  }
+}
