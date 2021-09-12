5ce0164b402ed7a33e16d7109dfbbdf66f1b8168
U190709050 <elf.prlk64@gmail.com>
2021-09-12 20:48:30 +0300

    Comments added

--- lib/main.dart
+++ lib/main.dart
@@ -18,17 +18,19 @@ class MyApp extends StatelessWidget {
 }
 
 class CounterPage extends StatefulWidget {
+  // Statefull widget kullanmamızdaki tek amaç kullanımı tamamlandıktan sonra stream'i kapatmak dispose metodunu çağırabilmek
   @override
   _CounterPageState createState() => _CounterPageState();
 }
 
 class _CounterPageState extends State<CounterPage> {
   int _counter = 0;
-  final StreamController<int> _streamController = StreamController<int>();
+  final StreamController<int> _streamController = StreamController<
+      int>(); // akışı kontrol edebileceğimiz bir controller tanımlıyoruz ve akıştaki veri türünün integer olduğunu söylüyoruz
 
   @override
   void dispose() {
-    _streamController.close();
+    _streamController.close(); //kapatmayı unutmmamalıyız
     super.dispose();
   }
 
@@ -38,16 +40,21 @@ class _CounterPageState extends State<CounterPage> {
       appBar: AppBar(title: Text('Stream Ile Counter')),
       body: Center(
         child: StreamBuilder<int>(
-            stream: _streamController.stream,
+            //Stream'i dinleyerek akıştan çıkan her yeni veriyi Text wigetını güncelleyerek yansıtıyoruz.
+            stream: _streamController
+                .stream, //StreamController ile stream'e ulaşıyoruz
             initialData: _counter,
             builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+              //builder içerisindeki snapshot parametresiyle akışımızdan çıka son verinin değerini Text widgetında yansıtıyoruz.
               return Text('Sayaç: ${snapshot.data}');
             }),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          _streamController.sink.add(++_counter);
+          _streamController.sink.add(
+              ++_counter); //FloatingActionButton'a bastığımızda counter değerinin bir artmış olarak sink sayesinde akışa yoluyoruz. 
+              //Stram'e yeni bir veri girişi akışı dinleyen StreamBuilder'ın widgetı yeni değeriyle beraber tekrar build etmesini sağlıyor.
         },
       ),
     );
